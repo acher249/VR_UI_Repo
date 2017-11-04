a1462b3c103e9a747f5efa14ab65ec0966b3e7a4
Adam Chernick <jchernick2010@gmail.com>
2017-11-04 16:15:28 -0400

    UI Update
    UI Update

---
+++ LearningAnimation/Assets/Scripts/MenuSliderController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSliderController : MonoBehaviour
+{
+
+    Animator anim;
+
+    public GameObject slidePanel;
+
+
+    public void OnTriggerEnter()
+    {
+        anim.SetBool("isOpening", true);
+    }
+
+    public void OnTriggerExit()
+    {
+        anim.SetBool("isOpening", false);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+        anim = slidePanel.transform.GetComponent<Animator>();
+        //anim = slidePanel.transform.GetComponent<>(); //try to turn off raycast target with script or find someway to make it not raycast hit whenanimates back...
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+}
